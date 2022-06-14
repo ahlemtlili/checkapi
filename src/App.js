ed5e50dd9c0b68d1564d253f29e2c5fde91ea4f0
ahlemtlili <ahlem.tlili@gmail.com>
2022-06-14 03:09:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import  UserList  from './UserList';
 
 function App() {
+
+  const [listOFUsers, setlistOFUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+  const getusers =async()=> {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+      setlistOFUsers(response.data);
+      console.log(response.data)
+      setLoading(false)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+      console.log("component app did mount or state query updated")
+    getusers()
+  }, [])
+
   return (
     <div className="App">
-     
+      <div className='header'>
+        <img src="https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/users5.png" alt="users" />
+        <h1>The Users are :</h1>
+      </div>
+      {loading?<h1>loading ...</h1>: <UserList users={listOFUsers}/>}
     </div>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table } from "react-bootstrap"
 
-const UserList = () => {
+const UserList = ({users}) => {
   return (
-    <div>UserList</div>
+
+    <div className='tableheader'>
+    <Table striped bordered hover variant="dark">
+        <thead>
+            <tr>
+                <th style={{width:'7%'}}>Id</th>
+                <th style={{width:'16.66%'}}>Name</th>
+                <th style={{width:'16.66%'}}>Username</th>
+                <th style={{width:'16.66%'}}>Email</th>
+                <th style={{width:'16.66%'}}>Website</th>
+                <th style={{width:'26.32%'}}>Address</th>
+            </tr>
+        </thead>
+    </Table>
+   {users.map((user) => (
+       <div> 
+            <Table className='tablebody'>
+                <tbody>
+                    <tr>
+                        <td style={{width:'7%', backgroundColor:'brown'}}>{user.id}</td>
+                        <td style={{width:'16.66%'}}>{user.name}</td>
+                        <td style={{width:'16.66%' }}>{user.username}</td>
+                        <td style={{width:'16.66%'}}>{user.email}</td>
+                        <td style={{width:'16.66%'}}>{user.website}</td>
+                        <td style={{width:'26.32%'}}>{user.address.street}, {user.address.city} </td>
+                    </tr>  
+                </tbody>
+            </Table>
+        </div> 
+       
+   ))}
+
+</div>
+
+  
   )
 }
 
